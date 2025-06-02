ce4791157916771f5d75e4424773a8e4b0cd2efb
Viacheslav Arkadev <mirage.phone@gmail.com>
2025-06-02 13:14:55 +0500

    [*] diploma/src/components/navigation/HeaderLeft.jsx

--- src/components/navigation/HeaderLeft.jsx
+++ src/components/navigation/HeaderLeft.jsx
@@ -1,22 +1,23 @@
-'use client'
-import { useRouter } from 'next/navigation';
-import {ChevronLeftIcon} from "lucide-react";
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import { ChevronLeftIcon } from "lucide-react";
+import React from "react";
+import { usePathname } from "next/navigation";
 
 const HeaderLeft = () => {
-    const router = useRouter()
-
-    return (
-      <div className='header-item header-left'>
-        <div
-          onClick={() => {
-            router.back();
-          }}
-        >
-          <ChevronLeftIcon className='icon' />
-        </div>
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <div className='header-item header-left'>
+      <div
+        onClick={() => {
+          router.back();
+        }}
+      >
+        {pathname !== "/" && pathname !== "/a" && <ChevronLeftIcon className='icon' />}
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default HeaderLeft;
